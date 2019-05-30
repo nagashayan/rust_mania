fa814714b00aafc567946688035b808ec42bab0c
Nagashayan <nagashayan1@gmail.com>
2019-05-29 20:05:04 -0400

    Revert "hello worlds"
    This reverts commit 75f3d31b1dea24d92518c2902d30b0892ceb90f3.

--- hello_cargo/target/debug/deps/hello_cargo-3524ceb8725d63a0.d
+++
@@ -1,5 +0,0 @@
-/Users/nagashayanaramamurthy/GitHub/rust_mania/hello_cargo/target/debug/deps/hello_cargo-3524ceb8725d63a0: src/main.rs
-
-/Users/nagashayanaramamurthy/GitHub/rust_mania/hello_cargo/target/debug/deps/hello_cargo-3524ceb8725d63a0.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/nagashayanaramamurthy/GitHub/rust_mania/hello_cargo/target/debug/hello_cargo.dSYM: /Users/nagashayanaramamurthy/GitHub/rust_mania/hello_cargo/src/main.rs
