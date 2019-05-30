75f3d31b1dea24d92518c2902d30b0892ceb90f3
Nagashayan <nagashayan1@gmail.com>
2019-05-29 20:01:28 -0400

    hello worlds

---
+++ hello_cargo/target/debug/deps/hello_cargo-3524ceb8725d63a0.d
@@ -0,0 +1,5 @@
+/Users/nagashayanaramamurthy/GitHub/rust_mania/hello_cargo/target/debug/deps/hello_cargo-3524ceb8725d63a0: src/main.rs
+
+/Users/nagashayanaramamurthy/GitHub/rust_mania/hello_cargo/target/debug/deps/hello_cargo-3524ceb8725d63a0.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/nagashayanaramamurthy/GitHub/rust_mania/hello_cargo/target/debug/hello_cargo.dSYM: /Users/nagashayanaramamurthy/GitHub/rust_mania/hello_cargo/src/main.rs
